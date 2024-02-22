086e599e94039be0ac8c975800dd3ccdda15e206
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-02-22 15:35:44 -0300

    feat: Implement 'Read More' button functionality in the JavaScript file

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,11 @@
 function Readmore(){
-    document.getElementById("paragraph-read").innerText = "Olá! Meu nome é Pedro, tenho 19 anos e sou natural de São Paulo. Minha jornada na programação teve início em 2020, em meio aos desafios da pandemia. Com esforço persistente e dedicação, alcancei um marco significativo ao ser aprovado na prova do SENAI Suíço-Brasileira. \n \n Atualmente, estou matriculado no curso técnico em Desenvolvimento de Sistemas, onde venho aprimorando minhas habilidades em HTML, CSS, JavaScript e Java. Ao integrar textos e links aos meus projetos, estou moldando ativamente meu caminho no fascinante universo da tecnologia. Junte-se a mim nesta emocionante jornada enquanto continuo aprendendo, criando e explorando as infinitas possibilidades no mundo do desenvolvimento."
+    reading = document.getElementById("read").innerText;
+
+    if(reading == "Read Less"){
+        document.getElementById("read"). innerText = "Read More"
+        document.getElementById("paragraph-read").innerText = "Meu nome é Pedro, tenho 19 anos e sou natural de São Paulo. Minha jornada na programação começou em 2020, durante a pandemia. Meu esforço e dedicação me levaram a fazer a prova do SENAI Suíço-Brasileira, na qual fui aprovado. Hoje, estou cursando o técnico em Desenvolvimento de Sistemas, continuando a aprimorar minhas habilidades e a construir meu caminho no emocionante mundo da tecnologia.";
+    }else{
+        document.getElementById("read"). innerText = "Read Less";
+        document.getElementById("paragraph-read").innerText = "Olá! Meu nome é Pedro, tenho 19 anos e sou natural de São Paulo. Minha jornada na programação teve início em 2020, em meio aos desafios da pandemia. Com esforço persistente e dedicação, alcancei um marco significativo ao ser aprovado na prova do SENAI Suíço-Brasileira. \n \n Atualmente, estou matriculado no curso técnico em Desenvolvimento de Sistemas, onde venho aprimorando minhas habilidades em HTML, CSS, JavaScript e Java. Ao integrar textos e links aos meus projetos, estou moldando ativamente meu caminho no fascinante universo da tecnologia. Junte-se a mim nesta emocionante jornada enquanto continuo aprendendo, criando e explorando as infinitas possibilidades no mundo do desenvolvimento.";
+    }
 }
